e3eca4b3cd901cebf7aa1e86ef5a39ed64123385
vladyslav.bashtannyk <vladyslav.bashtannyk@jooble.com>
2024-06-05 13:50:29 +0300

    Refactored

--- tbot/shell/monobank.py
+++ tbot/shell/monobank.py
@@ -1,14 +1,16 @@
 import os
+
 import django
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'money_manager.settings')
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "money_manager.settings")
 django.setup()
 
-import structlog
-from money_manager.config import config
-from tbot.controllers.integration import check_monobank
-from tbot_base.repository.bot_user import BotUserRepository
-from tbot_base.security.encrypting import EncryptManager
+import structlog  # noqa
+
+from money_manager.config import config  # noqa
+from tbot.controllers.integration import check_monobank  # noqa
+from tbot_base.repository.bot_user import BotUserRepository  # noqa
+from tbot_base.security.encrypting import EncryptManager  # noqa
 
 logger = structlog.get_logger(__name__)
 
@@ -18,13 +20,15 @@ def refresh_monobank_webhooks(dsn: str) -> None:
     for user in BotUserRepository.select(first=False):
         for integration in user.integrations.all():
             if not check_monobank(
-                    dsn=dsn,
-                    mono_token=encrypt_manager.decrypt_key(integration.monobank_token),
-                    chat_id=int(user.user_id),
-                    encrypted_user_id=encrypt_manager.encrypt_key(str(user.user_id)),
-                    base_url=config.monobank.base_url,
+                dsn=dsn,
+                mono_token=encrypt_manager.decrypt_key(integration.monobank_token),
+                chat_id=int(user.user_id),
+                encrypted_user_id=encrypt_manager.encrypt_key(str(user.user_id)),
+                base_url=config.monobank.base_url,
             ):
-                logger.warning("Failed to refresh monobank webhooks", user_id=user.user_id)
+                logger.warning(
+                    "Failed to refresh monobank webhooks", user_id=user.user_id
+                )
 
 
 def run():

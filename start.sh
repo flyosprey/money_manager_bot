609e01929abf80ac5858e1b0bb08690f4e13d8eb
vladyslav.bashtannyk <vladyslav.bashtannyk@jooble.com>
2024-09-11 16:21:35 +0300

    Added possibility create specific django superuser

--- start.sh
+++ start.sh
@@ -10,12 +10,25 @@ python manage.py makemigrations
 echo "Migrate..."
 python manage.py migrate
 
+SUPERUSER_NAME=${DJANGO_SUPERUSER_USERNAME}
+SUPERUSER_EMAIL=${DJANGO_SUPERUSER_EMAIL}
+SUPERUSER_PASSWORD=${DJANGO_SUPERUSER_PASSWORD}
+
 echo "Create superuser if not exist..."
 python manage.py shell <<EOF
 from django.contrib.auth import get_user_model
+import os
+
+
 User = get_user_model()
-if not User.objects.filter(username='admin').exists():
-    User.objects.create_superuser('admin', 'admin@example.com', 'adminpassword')
+
+username = os.getenv('DJANGO_SUPERUSER_USERNAME', '$SUPERUSER_NAME')
+email = os.getenv('DJANGO_SUPERUSER_EMAIL', '$SUPERUSER_EMAIL')
+password = os.getenv('DJANGO_SUPERUSER_PASSWORD', '$SUPERUSER_PASSWORD')
+
+if not User.objects.filter(username=username).exists():
+    User.objects.create_superuser(username=username, email=email, password=password)
+    print('Superuser created')
 EOF
 
 echo "Starting Django server..."

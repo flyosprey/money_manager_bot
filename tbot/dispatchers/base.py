19967abc25cd2a3002e6ec1e3747c3090d3c583b
vladyslav.bashtannyk <vladyslav.bashtannyk@jooble.com>
2024-10-23 09:56:10 +0300

    Updated start message

--- tbot/dispatchers/base.py
+++ tbot/dispatchers/base.py
@@ -11,7 +11,11 @@ def handle_start(message: Message):
     register_user(message=message)
     bot.send_message(
         chat_id=message.chat.id,
-        text="Привіт!👋\nОбери опцію",
+        text="Привіт!👋 Я бот OspreyMoney."
+             "\nЯ допоможу тобі автоматично фіксувати прибутки та витрати з твоїх кредитних карток. "
+             "💳Наразі я працюю з Monobank і записую всі твої витрати у додаток BudgetBakers. "
+             "Зі мною ти завжди будеш контролювати свій бюджет швидко та зручно!"
+             "\n🧾Обери опцію нижче, щоб розпочати.",
         parse_mode="HTML",
         reply_markup=menu(bot),
     )

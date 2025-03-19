810ca855ae1bf44475d1175ba2803997483a33a1
vladyslav <vlad.bashtannyk11@gmail.com>
2025-03-19 11:57:47 +0200

    Updated error handling (GitHub) Updated skip_transaction

--- tbot_base/views.py
+++ tbot_base/views.py
@@ -134,7 +134,7 @@ class MonobankWebhookView(View):
 
     @staticmethod
     def skip_transaction(transaction: Transaction) -> bool:
-        return bool(re.search(r"з \w+ картки", transaction.description.lower()))
+        return bool(re.search(r"з.+?(картки|ФОП)|на.+(картку|ФОП)", transaction.description.lower()))
 
 
 @method_decorator(csrf_exempt, name="dispatch")
@@ -177,11 +177,12 @@ class GithubWebhookView(View):
             logger.error(e)
             return JsonResponse({"error": "Permission denied"}, status=403)
         except DeployError as e:
+            logger.exception(e)
             admin_bot_notification(message=f"Failed to deploy new version!🔴\n{e}")
-            return JsonResponse({"status": "failure", "error": str(e)}, status=500)
+            return JsonResponse({"status": "failure", "error": "Internal error"}, status=500)
         except Exception as e:
             logger.exception(e)
-            return JsonResponse({"status": "failure", "error": str(e)}, status=500)
+            return JsonResponse({"status": "failure", "error": "Internal error"}, status=500)
 
     @staticmethod
     def verify_signature(

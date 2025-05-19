b863c3c5567c0d5187c577c52e947fc7889204c2
vladyslav <vlad.bashtannyk11@gmail.com>
2025-05-19 21:06:01 +0300

    Added ngrok to compose yml

--- start.sh
+++ start.sh
@@ -34,7 +34,7 @@ if not User.objects.filter(username=username).exists():
 EOF
 
 echo "Starting Django server..."
-gunicorn --reload money_manager.wsgi:application --bind 0.0.0.0:8000 --workers 3 --timeout 120 &
+gunicorn --reload money_manager.wsgi:application --bind 0.0.0.0:${WEB_PORT} --workers 3 --timeout 120 &
 
 sleep 20
 

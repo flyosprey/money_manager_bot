09f71fcf78fa72088956224a3349029a44aa543e
vladyslav <vlad.bashtannyk11@gmail.com>
2025-05-21 17:14:59 +0300

    Fixed deleting doc_id from memory Added UserTransactions to admin

--- money_manager/settings.py
+++ money_manager/settings.py
@@ -87,8 +87,8 @@ TEMPLATES = [
 
 WSGI_APPLICATION = "money_manager.wsgi.application"
 
-SECURE_PROXY_SSL_HEADER = ("HTTP_X_FORWARDED_PROTO", "http")
-SECURE_SSL_REDIRECT = False
+SECURE_PROXY_SSL_HEADER = ("HTTP_X_FORWARDED_PROTO", "https")
+SECURE_SSL_REDIRECT = True
 
 # Database
 # https://docs.djangoproject.com/en/2.2/ref/settings/#databases
